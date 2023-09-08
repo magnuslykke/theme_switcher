f3f8ee0506278d2da927b67557365336d6fd4742
magnuslykke <magn965m@stud.kea.dk>
2023-09-08 09:49:17 +0200

    tilføjet localStorage

--- script2.js
+++ script2.js
@@ -1,11 +1,19 @@
 "use strict";
 
+window.addEventListener("load", themeListener);
+
+function themeListener() {
+  // const getItem = localStorage.getItem("theme");
+  document.querySelector("body").dataset.theme = localStorage.getItem("theme");
+}
+
 document.querySelectorAll("option").forEach((button) => {
   addEventListener("click", klik);
 });
 
 function klik(evt) {
   console.log("Der er klikket på select knappen");
+  localStorage.setItem("theme", `${evt.target.value}`);
   if (evt.target.value === "light") {
     document.querySelector("body").dataset.theme = "light";
   } else if (evt.target.value === "dark") {
